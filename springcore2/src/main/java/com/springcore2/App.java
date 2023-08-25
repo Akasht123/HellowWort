e1d60fbc2d2e926ef6f9d43cbc05afeadd99ae88
Akash <akasht1609@gmail.com>
2023-08-25 12:58:21 -0800

    Initial commit

---
+++ springcore2/src/main/java/com/springcore2/App.java
@@ -0,0 +1,20 @@
+package com.springcore2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student stu = (Student)context.getBean("stu");
+        System.out.println(stu);
+    }
+}
